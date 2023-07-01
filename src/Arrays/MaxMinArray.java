2141d284229206eaa5e8e1f46a2130883e4fab43
Dhruvin Patel <dhruvinpatel1409@gmail.com>
2023-07-01 10:26:45 +0530

    Shradha Didi and Aman Bhaiya DSA Problems

--- src/Arrays/MaxMinArray.java
+++ src/Arrays/MaxMinArray.java
@@ -1,4 +1,35 @@
 package Arrays;
 
-public class Problem1 {
+import java.util.Scanner;
+
+public class MaxMinArray
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+
+        int size;
+        System.out.print("Enter Size of an Array : ");
+        size = scan.nextInt();
+
+        int input[] = new int[size];
+
+        for(int i=0; i<size; i++)
+        {
+            System.out.print("Enter Element-" +i +" : ");
+            input[i] = scan.nextInt();
+        }
+
+        int max = input[0], min = input[0];
+        for(int i=0; i<size; i++)
+        {
+            if(max<input[i])
+                max = input[i];
+            if(min>input[i])
+                min = input[i];
+        }
+
+        System.out.println("\n\nMaximum Number : " +max);
+        System.out.println("Minimum Number : " +min);
+    }
 }
--- src/Arrays/ReverseArray.java
+++ src/Arrays/ReverseArray.java
@@ -1,2 +1,33 @@
-package Arrays;public class ReverseArray {
-}
+//package Arrays;
+//
+//import java.util.Scanner;
+//
+//public class ReverseArray
+//{
+//    int k=0;
+//    public void reversePrint(int x[])
+//    {
+//        System.out.print(reversePrint(x[k+1])+ "" +x[k]);
+//    }
+//
+//    public static void main(String args[])
+//    {
+//        Scanner scan = new Scanner(System.in);
+//
+//
+//        int size, max;
+//        System.out.print("Enter Size of an Array : ");
+//        size = scan.nextInt();
+//
+//        int input[] = new int[size];
+//
+//        for(int i=0; i<size; i++)
+//        {
+//            System.out.print("Enter Element-" +i +" : ");
+//            input[i] = scan.nextInt();
+//        }
+//
+//        System.out.print("\n\nReversed Array : ");
+//
+//    }
+//}
